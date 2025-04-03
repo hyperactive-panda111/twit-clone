1e2c528cf61d0f348667322a2545d5383e3d5199
amomensah675 <amomensah675@gmail.com>
2025-04-03 21:59:56 +0000

    add Image component

---
+++ src/app/components/Image.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { IKImage } from 'imagekitio-next';
+
+type ImageType = {
+	path: string;
+	w?: number;
+	h?: number;
+	alt: string;
+	className?: string;
+	tr?: boolean; 
+};
+
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
+
+const Image = ({ path, w, h, alt, className, tr }: ImageType) => {
+    return (
+    <IKImage 
+        urlEndpoint={urlEndpoint}
+        path={path}
+        width={w}
+        height={h}
+        alt={alt}
+        className={className}
+        {...(tr
+             ? { transformation: [{ width: `${w}`, height: `${h}`}]}
+             : { width: w, height: w } )}
+    />
+    );
+};
+
+export default Image;
