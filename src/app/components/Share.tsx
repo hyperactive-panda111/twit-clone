3d865b4e61500295223178642153732852ba0c47
amomensah675 <amomensah675@gmail.com>
2025-04-05 08:49:39 +0000

    add image editing functionality to Share component

--- src/app/components/Share.tsx
+++ src/app/components/Share.tsx
@@ -4,9 +4,18 @@ import { useState } from 'react';
 import Image from './Image';
 import NxtImage from 'next/image';
 import { shareAction } from '../../../actions';
+import { ImageEditor } from './ImageEditor';
 
 const Share = () => {
   const [media, setMedia] = useState<File | null>(null);
+  const [isEditorOpen, setIsEditorOpen] = useState(false);
+  const [settings, setSettings] = useState<{
+    type: 'original' | 'wide' | 'square';
+    sensitive: boolean;
+  }>({
+    type: 'original',
+    sensitive: false,
+  })
 
   const handleMediaChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
@@ -18,7 +27,7 @@ const Share = () => {
   
   console.log(`previewURL: ${previewURL}`);
   return (
-    <form className='p-4 flex gap-4' action={shareAction}>
+    <form className='p-4 flex gap-4' action={formData => shareAction(formData,settings)}>
       <div className='relative w-10 h-10 rounded-full overflow-hidden'>
         <Image
           path='general/panda.png'
@@ -32,9 +41,28 @@ const Share = () => {
         <input type='text' name='desc'placeholder='What is happening?!' className='bg-transparent outline-none placeholder:text-textGray text-xl'/>
         {
           previewURL && <div className='relative rounded-xl overflow-hidden'>
-            <NxtImage src={previewURL} alt='preview-image' width={600} height={600} />
-            <div className='absolute'></div>
+            <NxtImage 
+              src={previewURL}
+              alt='preview-image'
+              width={600} 
+              height={600}
+              className={`w-full ${settings.type === 'original'
+                ? 'h-full object-contain'
+                : settings.type === 'square'
+                  ? 'aspect-square object-cover' : 'aspect-video object-cover'
+                }`} />
+            <div className='absolute top-2 left-2 bg-black bg-opacity-50 px-4 py-1 text-white text-sm font-bold cursor-pointer rounded-full' onClick={() => setIsEditorOpen(true)}>Edit</div>
           </div>
+        }
+        {
+          isEditorOpen && previewURL && (
+            <ImageEditor 
+              onClose={() => setIsEditorOpen(false)}
+              previewURL={previewURL}
+              settings={settings}
+              setSettings={setSettings}
+            />
+          )
         }
         <div className='flex items-center justify-between gap-4 flex-wrap'>
           <div className='flex items-center gap-4 flex-wrap'>
