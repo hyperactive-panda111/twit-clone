b8dcae1887a98effaf16a36afbccbb952f13910d
hyperactive-panda111 <111157902+hyperactive-panda111@users.noreply.github.com>
2025-04-11 23:44:56 +0000

    add db requests for posts data

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -6,6 +6,8 @@ export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams;
 
     const userProfileId = searchParams.get('user');
+    const page = searchParams.get('cursor');
+    const LIMIT = 3;
     
     const { userId } = await auth();
 
@@ -16,14 +18,17 @@ export async function GET(request: NextRequest) {
     // const ids = followings.map(f => f.followingId);
     // console.log('Response from DB: ', ids);
 
-    const whereCondition = userProfileId ? { parentPostId: null, userId: userProfileId } : {
+    const whereCondition = userProfileId !== 'undefined' ? { parentPostId: null, userId: userProfileId as string } : {
         parentPostId: null,
         userId: {
             in: [userId, ...(await prisma.follow.findMany({ where: { followerId: userId }, select: { followingId: true } })).map(f => f.followingId)]
         }
     };
 
-    const posts = await prisma.post.findMany({ where: whereCondition });
+    const posts = await prisma.post.findMany({ where: whereCondition, take: LIMIT, skip: (Number(page) - 1) * LIMIT});
+    const totalPosts = await prisma.post.count({ where: whereCondition });
+    const hasMore = Number(page) * LIMIT < totalPosts;
 
-    return Response.json(posts);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    return Response.json({ posts, hasMore });
 }
