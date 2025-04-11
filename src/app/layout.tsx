5e8f4d6bdee1b60b11efd194a6c2b52680bb3cf1
hyperactive-panda111 <111157902+hyperactive-panda111@users.noreply.github.com>
2025-04-11 23:41:29 +0000

    add QueryProvider to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 import type { Metadata } from 'next'
+import { QueryClientProvider } from "@tanstack/react-query";
+import QueryProvider from "@/providers/QueryProvider";
 
 export const metadata: Metadata = {
   title: 'X Clone',
@@ -17,11 +19,13 @@ export default function AppLayout({
 }>) {
   return (
     <ClerkProvider>
+      <QueryProvider>
       <html lang="en">
         <body>
           {children}
         </body>
       </html>
+      </QueryProvider>
     </ClerkProvider>
   );
 }
