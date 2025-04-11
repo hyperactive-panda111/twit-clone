bc572d295d093488f2b0ec344818a86e43e1054e
hyperactive-panda111 <111157902+hyperactive-panda111@users.noreply.github.com>
2025-04-11 23:40:12 +0000

    implement inifite scroll functionality by making requests on client component

--- src/components/InfiniteFeed.tsx
+++ src/components/InfiniteFeed.tsx
@@ -1,10 +1,42 @@
 'use client';
 
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Post from "./Post";
+
+const fetchPosts = async (pageParam: number, userProfileId?: string ) => {
+  const res = await fetch('/api/posts?cursor='+pageParam+'&user='+userProfileId);
+  return res.json();
+};
+
 const InfiniteFeed = ({ userProfileId }: {userProfileId?: string }) => {
-  
+  const {data, error, hasNextPage, fetchNextPage, status } = useInfiniteQuery({
+    queryKey: ['posts'],
+    queryFn: ({ pageParam=2})=>fetchPosts(pageParam, userProfileId),
+    initialPageParam: 2,
+    getNextPageParam: (lastPage, pages)=> lastPage.hasMore ? pages.length + 2 : undefined,
+  });
+
+  if (error) return 'Something went wrong';
+  if (status === 'pending') return 'Loading...';
+
+  // console.log("Data from useInfiniteQuery hook:", data);
+  const allPosts = data?.pages?.flatMap(page=>page.posts) || [];
+
+
   return (
-    <div>InfiniteFeed</div>
+    <InfiniteScroll 
+      dataLength={allPosts.length}
+      next={fetchNextPage}  
+      hasMore={!!hasNextPage}  
+      loader={<h1>Posts are loading...</h1>}
+      endMessage={<h1>All Posts Received</h1>} 
+    >
+    {allPosts.map((post) => (
+      <Post key={post.id} />
+    ))}
+    </InfiniteScroll>
   )
 }
 
-export default InfiniteFeed
+export default InfiniteFeed;
