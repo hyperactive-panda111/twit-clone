fc7326029a7f760feaba47718d1c03142241dda1
hyperactive-panda111 <111157902+hyperactive-panda111@users.noreply.github.com>
2025-04-12 10:27:50 +0000

    add layout changes to Post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -5,107 +5,112 @@ import PostInteractions from "./PostInteractions";
 import Video from "./Video";
 import Link from "next/link";
 import { Post as PostType } from "../../prisma/db/generated/prisma";
+import { format } from "timeago.js";
 
-interface FileDetailsResponse {
-    width: number;
-    height: number;
-    filePath: string;
-    url: string;
-    fileType: string;
-    customMetadata?: {
-        sensitive: boolean;
+type PostWithDetails = PostType & {
+    user: {
+        displayName: string | null;
+        img: string | null;
+        username: string;
+    };
+    rePost?: PostType & {
+        user: {
+            displayName: string | null;
+            img: string | null;
+            username: string;
+        };
     }
 };
 
-const Post = ({ type }: { type ?: 'status' | 'comment'}) => {
-    // const getFileDetails = async (fileId: string): Promise<FileDetailsResponse> => {
-    //     return new Promise((resolve, reject) => {
-    //         imagekit.getFileDetails(fileId, function (error, result) {
-    //             if (error) reject(error);
-    //             else resolve(result as FileDetailsResponse);
-    //         });
-    //     });
-    // }
-// 67f16b9d432c4764160e9ae8 67f190a8432c476416e134a8 67f24698432c476416b0319d
-    // const fileDetails = await getFileDetails('67f24698432c476416b0319d');
-
-
+const Post = ({ type, post }: { type ?: 'status' | 'comment', post: PostWithDetails}) => {
+    
+    const originalPost = post.rePost || post;
     return (
         <div className='p-4 border-y-[1px] border-borderGray'>
             {/* POST TYPE */}
+            {post.rePostId && (
             <div className='flex gap-2 items-center text-sm text-textGray mb-2 font-bold'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
                     <path fill="#71767b"
                         d="M4.75 3.79l4.603 4.3-1.706 1.82L6 8.38v7.37c0 .97.784 1.75 1.75 1.75H13V20H7.75c-2.347 0-4.25-1.9-4.25-4.25V8.38L1.853 9.91.147 8.09l4.603-4.3zm11.5 2.71H11V4h5.25c2.347 0 4.25 1.9 4.25 4.25v7.37l1.647-1.53 1.706 1.82-4.603 4.3-4.603-4.3 1.706-1.82L18 15.62V8.25c0-.97-.784-1.75-1.75-1.75z"
                     />
                 </svg>
-                <span className="">KarmaDev reposted</span>
-            </div>
+                <span className="">{post.user.displayName} reposted</span>
+            </div>)}
+            
             {/* POST CONTENT */}
-            <div className={`${type === 'status' ? 'flex flex-col gap-4' : ''}`}>
-                {/* AVATAR */}
-                <div className={`${type === 'status' && 'hidden'} relative w-10 h-10 rounded-full overflow-hidden`}>
-                    <Image
-                        path="/general/panda.png"
-                        alt="profile"
-                        tr
-                        w={100}
-                        h={100}
-                    />
+            <div className="flex gap-4">
+                {/* AVATAR COLUMN */}
+                <div className={`${type === 'status' ? 'hidden' : ''}`}>
+                    <div className="relative w-10 h-10 rounded-full overflow-hidden">
+                        <Image
+                            //@ts-ignore
+                            path={!!originalPost.user.img && '/general/noprofile.jpg'}
+                            alt="profile"
+                            tr={true}
+                            w={100}
+                            h={100}
+                        />
+                    </div>
                 </div>
-                {/* CONTENT */}
-                <div className="flex-1 flex flex-col gap-2">
-                    {/* TOP */}
+                
+                {/* MAIN CONTENT COLUMN */}
+                <div className="flex-1 flex flex-col gap-2 max-w-full overflow-hidden">
+                    {/* USER INFO ROW */}
                     <div className='w-full flex justify-between'>
                         <Link href={`/test`} className="flex gap-4">
-                            <div className={`${type !== 'status' && 'hidden'} relative w-10 h-10 rounded-full overflow-hidden`}>
-                                <Image
-                                    path="/general/panda.png"
-                                    alt="profile"
-                                    tr={true}
-                                    w={100}
-                                    h={100}
-                                />
-                            </div>
-                            <div className={`flex gap-2 items-center flex-wrap ${type === 'status' && 'flex-col gap-0 !items-start'}`}>
-                                <h1 className={`text-md text-bold ${type==='status' && 'text-sm'}`}>Karma Dev</h1>
-                                <span className="text-textGray">@karmadev</span>
-                                {type !== 'status' && <span className="text-textGray">1 day ago</span>}
+                            {/* AVATAR - shown only when status */}
+                            {type === 'status' && (
+                                <div className="relative w-10 h-10 rounded-full overflow-hidden">
+                                    <Image
+                                        path={originalPost.user.img || "/general/panda.png"}
+                                        alt="profile"
+                                        tr={true}
+                                        w={100}
+                                        h={100}
+                                    />
+                                </div>
+                            )}
+                            
+                            <div className={`flex gap-2 items-center flex-wrap ${type === 'status' ? 'flex-col gap-0 !items-start' : ''}`}>
+                                <h1 className={`text-md text-bold ${type === 'status' ? 'text-sm' : ''}`}>{originalPost.user.displayName}</h1>
+                                <span className="text-textGray">@{originalPost.user.username}</span>
+                                {type !== 'status' && <span className="text-textGray">{format(originalPost.createdAt)}</span>}
                             </div>
                         </Link>
-                         <PostInfo />
-                        </div>
+                        <PostInfo />
                     </div>
-                    {/* TEXT & MEDIA */}
-                    <Link href={`/test/status/12`}>
-                        <p className={`${type === 'status' && 'text-lg'}`}>
-                            Lorem ipsum dolor sit amet consectetur
-                            adipisicing elit. Repellendus maiores animi
-                            vitae temporibus, inventore, rerum excepturi
-                            distinctio voluptatem voluptate laboriosam at!
-                            Assumenda tempore cum, ipsum doloremque
-                            consectetur debitis vel quaerat.
-                        </p>
-                    </Link>
-                    <Image path="/general/post.jpeg" w={600} h={600} alt="post" />
-                    {/* {fileDetails && fileDetails.fileType === 'image' ? (
-                        <Image 
-                            className={fileDetails?.customMetadata?.sensitive ? 'blur-lg' : ''} 
-                            alt='' 
-                            path={fileDetails.filePath} 
-                            w={fileDetails.width} 
-                            h={fileDetails.height}
-                        />
-                    ) : (
-                            <Video 
-                                path={fileDetails.filePath}
-                                className={fileDetails?.customMetadata?.sensitive ? 'blur-lg' : ''} 
+                    
+                    {/* POST TEXT */}
+                    <div className="w-full break-words">
+                        <Link href={`/test/status/12`} className="inline-block w-full">
+                            <p className={`${type === 'status' ? 'text-lg' : ''} whitespace-normal break-words`}>
+                                {originalPost.desc}
+                            </p>
+                        </Link>
+                    </div>
+                    
+                    {/* POST MEDIA */}
+                    {originalPost.img && (
+                        <div className="mt-2 w-full rounded-xl overflow-hidden">
+                            <Image 
+                                //@ts-ignore
+                                path={!!originalPost.img && '/general/arcane2.jpg'} 
+                                w={600} 
+                                h={600} 
+                                alt="post" 
                             />
-                        )} */}
+                        </div>
+                    )}
+                    
+                    {/* TIMESTAMP FOR STATUS */}
                     {type === 'status' && <span className='text-textGray'>8:41 PM · Dec 5, 1886</span>}
+                    
+                    {/* INTERACTIONS */}
                     <PostInteractions />
                 </div>
             </div>
+        </div>
     );
 };
 
