3e3ad15ffe883b903d4a59ef67d933a499af8b1c
amomensah675 <amomensah675@gmail.com>
2025-04-13 21:51:28 +0000

    add db queries in the Feed component

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -4,38 +4,61 @@ import { auth } from '@clerk/nextjs/server';
 import InfiniteFeed from './InfiniteFeed';
 
 const Feed = async ({ userProfileId }: { userProfileId?: string }) => {
+  console.log("userProfileId: ",userProfileId);
+
   const { userId } = await auth();
+
   
   if (!userId) return;
-  console.log('userId: ', userId);
+  console.log('userId in feed comp: ', userId);
 
   // const followings = await prisma.follow.findMany({ where: {followerId: userId}, select: {followingId: true}});
   // const ids = followings.map(f => f.followingId);
   // console.log('Response from DB: ', ids);
 
-  const whereCondition = userProfileId ? { parentPostId: null, userId: userProfileId } : {
+  const whereCondition = userProfileId !== 'undefined' ? { parentPostId: null, userId: userProfileId  as string} : {
     parentPostId: null,
     userId: {
-      in: [userId, ...(await prisma.follow.findMany({ where: {followerId: userId, }, select: {followingId: true}})).map(f => f.followingId)]
+      in: [userId, ...(await prisma.follow.findMany({ where: {followerId: userId }, select: {followingId: true}})).map(f => f.followingId)]
     }
   };
 
-  const posts = await prisma.post.findMany({ where: whereCondition, take: 3, skip: 0, orderBy: { createdAt: 'desc'}});
+  const posts = await prisma.post.findMany({ 
+    where: whereCondition,
+    include: {
+      user: {select: { displayName: true, username: true, img:true }},
+      rePost: {
+        include: {
+          user: { select: { displayName: true, username: true, img: true }},
+          _count: {select: {likes: true, rePosts: true, comments: true }},
+          likes: { where: { userId: userId }, select: {id: true }},
+          rePosts: { where: { userId: userId}, select: { id: true }},
+          saves: { where: { userId: userId}, select: { id: true }},
+
+        },
+      },
+      _count: {select: { likes: true, rePosts: true, comments: true }},
+      likes: { where: { userId: userId }, select: {id: true }}, 
+      rePosts: { where: { userId: userId}, select: { id: true }},
+      saves: { where: { userId: userId}, select: { id: true }},
+    },
+    take: 3, 
+    skip: 0, 
+    orderBy: { createdAt: 'desc'}});
   console.log("DB Response for Posts request: ", posts);
   
   return (
     <div>
       {posts.map((post) => (
         <div key={post.id}>
-          <Post/>
-          FROM SERVER
+          <Post post={post}/>
         </div>
       ))}
-      <InfiniteFeed />
+      <InfiniteFeed userProfileId={userProfileId}/>
     </div>
 
   )
 };
 
 
-export default Feed
+export default Feed;
