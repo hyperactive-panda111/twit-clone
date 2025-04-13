97b3fffe2ec01c4b9c0618f85822f89ac0fc6bc8
hyperactive-panda111 <111157902+hyperactive-panda111@users.noreply.github.com>
2025-04-13 15:46:24 +0000

    add misc. changes and packages (timeagojs)

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
         hostname: "ik.imagekit.io",
         port: "",
       },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+      }
     ],
   },
   experimental: {
