fb4f9513f4d57233c5b86dd660ac9cd20d7e459a
amomensah675 <amomensah675@gmail.com>
2025-04-14 15:36:52 +0000

    add db query for specific post information

--- src/app/(board)/[username]/status/[postId]/page.tsx
+++ src/app/(board)/[username]/status/[postId]/page.tsx
@@ -2,8 +2,42 @@ import Link from 'next/link'
 import React from 'react'
 import Image from '@/components/Image';
 import Post from '@/components/Post';
+import prisma from '../../../../../../lib/prisma';
+import { auth } from '@clerk/nextjs/server';
+import { notFound } from 'next/navigation';
+import Comments from '@/components/Comments';
 
-const StatusPage = () => {
+const StatusPage = async ({ params}: { params: Promise<{ username: string, postId: string }>}) => {
+    
+    const { userId } = await auth();
+    const postId = (await params).postId;
+    if (!postId) return;
+    
+    if (!userId)
+    if (!userId) return;
+
+    const post = await prisma.post.findFirst({
+        where: { id: Number(postId)},
+        include: {
+            user: {select: { displayName: true, username: true, img:true }},
+            _count: {select: { likes: true, rePosts: true, comments: true }},
+            likes: { where: { userId: userId }, select: {id: true }}, 
+            rePosts: { where: { userId: userId}, select: { id: true }},
+            saves: { where: { userId: userId}, select: { id: true }},
+            comments: {
+                include: {
+                    user: {select: { displayName: true, username: true, img:true }},
+                    _count: {select: { likes: true, rePosts: true, comments: true }},
+                    likes: { where: { userId: userId }, select: {id: true }}, 
+                    rePosts: { where: { userId: userId}, select: { id: true }},
+                    saves: { where: { userId: userId}, select: { id: true }},
+            },
+          },
+        },
+});
+
+    if (!post) return notFound();
+
     return (
         <div>
             <div className=''>
@@ -13,7 +47,12 @@ const StatusPage = () => {
                     </Link>
                     <h1 className='font-bold text-lg'>Post</h1>
                 </div>
-                    <Post type='status'/>
+                    <Post post={post} type='status'/>
+                    <Comments 
+                        postId={post.id} 
+                        comments={post.comments}
+                        username={post.user.username}
+                     />
             </div>
         </div>
     )
