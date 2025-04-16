3c143001fb3889a2d650646cf6e58651c02d094f
amomensah675 <amomensah675@gmail.com>
2025-04-16 05:17:40 +0000

    add addPost, addComment, likePost, followUser, rePost, savePost server action

--- src/action.ts
+++ src/action.ts
@@ -2,6 +2,39 @@
 
 import { auth } from "@clerk/nextjs/server";
 import prisma from "../lib/prisma";
+import {z} from 'zod';
+import { revalidatePath } from "next/cache";
+import { imagekit } from "./utils";
+import { UploadResponse } from "imagekit/dist/libs/interfaces";
+
+
+export const followUser = async (targetUserId: string) => {
+    const { userId } = await auth();
+
+    if (!userId) return;
+
+    const existingFollow = await prisma.follow.findFirst({
+        where: { 
+            followerId: userId,
+            followingId: targetUserId,
+        },
+    });
+
+    if (existingFollow) {
+        await prisma.follow.delete({
+            where: {
+                id: existingFollow.id,
+            },
+        });
+    } else {
+        await prisma.follow.create({
+            data: {
+                followerId: userId,
+                followingId: targetUserId,
+            },
+        })
+    }
+};
 
 export const likePost = async (postId: number) => {
     const { userId } = await auth();
@@ -88,6 +121,129 @@ export const savePost = async (postId: number) => {
     }
 };
 
-export const addComment = async () => {
+export const addComment = async (prevState: { success: boolean, error: boolean}, formData: FormData) => {
+    const { userId } = await auth();
 
+    if (!userId) return { success: false, error: true };
+
+    const desc = formData.get('desc');
+    const postId = formData.get('postId');
+    const username = formData.get('username');
+
+    const Comment = z.object({
+        parentPostId: z.number(),
+        desc: z.string().max(225),
+    });
+
+    const validatedFields = Comment.safeParse({
+        parentPostId: Number(postId),
+        desc: desc,
+    });
+
+    if (!validatedFields.success) {
+        return { success: false, error: true };
+    }
+
+    try {
+        await prisma.post.create({
+            data: {
+                ...validatedFields.data,
+                userId: userId,
+            },
+        });
+        revalidatePath(`/${username}/status/${postId}`);
+        return { success: true, error: false };
+    } catch (error) {
+        console.log(error);
+        return { success: false, error: true };
+    }
+};
+
+export const addPost = async (
+    state: { success: boolean; error: boolean},
+    formData: FormData
+) => {
+    const { userId } = await auth();
+
+    if (!userId) return { success: false, error: true };
+
+    const desc = formData.get('desc');
+    const file = formData.get('file') as File;
+    const isSensitive = formData.get('isSensitive') as string;
+    const imgType = formData.get('imgType');
+
+    const uploadFile = async (file: File): Promise<UploadResponse> => {
+        const bytes = await file.arrayBuffer();
+	    const buffer = Buffer.from(bytes);
+
+        const transformation = `w-600,${imgType === 'square' 
+            ? 'ar-1-1'
+            : imgType === 'wide'
+            ? 'ar-16-9' 
+            : ''
+        }`
+
+        return new Promise((resolve, reject)=> {
+            imagekit.upload(
+            {
+                file: buffer,
+                fileName: file.name,
+                folder: '/posts',
+                ...(file.type.includes("image") && {
+                    transformation: {
+                    pre: transformation,
+        },
+            }),
+        }, function(error, result) {
+            if(error) reject(error);
+            else resolve(result as UploadResponse);
+        });
+        })
+    };
+
+    const Post = z.object({
+        desc: z.string().max(225),
+        isSensitive: z.boolean().optional(),
+    });
+
+    const validatedFields = Post.safeParse({
+        desc: desc,
+        isSensitive: JSON.parse(isSensitive)
+    });
+
+    if (!validatedFields.success) {
+        return { success: false, error: true };
+    }
+
+    let img = '';
+    let imgHeight = 0;
+    let video = '';
+
+    if (file.size) {
+        const result: UploadResponse = await uploadFile(file);
+
+        if (result.fileType === 'image'){
+            img = result.filePath;
+            imgHeight = result.height;
+        } else {
+            video = result.filePath;
+        }
+    }
+
+    try {
+        await prisma.post.create({
+            data: {
+                ...validatedFields.data,
+                userId,
+                img,
+                imgHeight,
+                vid: video,
+            },
+        });
+        revalidatePath(`/`);
+        return { success: true, error: false };
+    } catch (error) {
+        console.log(error);
+        return { success: false, error: true };
+    }
 };
